9425656ed27e55060dfdbfcbf8d7ecee475a3e45
ceslep <ceslep@users.noreply.github.com>
2026-06-05 12:57:14 -0500

    version

--- src/version.ts
+++ src/version.ts
@@ -7,8 +7,8 @@
 
 import Swal from "sweetalert2";
 
-export const APP_VERSION = "1.1.1";
-export const APP_BUILD_DATE = "2026-05-28";
+export const APP_VERSION = "1.2.1";
+export const APP_BUILD_DATE = "2026-06-05";
 
 const VERSION_STORAGE_KEY = "app_version";
 let updateAvailable = false;
